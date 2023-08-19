dc955d8c5d23d8e17dfc69a83fa3fd50c4a404e9
Do Thanh Phuoc <dothanhphuoc0706@gmail.com>
2023-08-19 16:01:59 +0700

    Hoan thieu chuc nang dang nhap REACTJS vs NODEJS (co su dung REDUX)

--- src/server.js
+++ src/server.js
@@ -4,17 +4,23 @@ import viewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 import connectDB from './configs/connectDB';
 import dotenv from "dotenv/config";
+import cors from "cors";
 
 let app = express();
+
+//cors
+app.use(cors({ credentials: true, origin: true }));
 
 //config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 viewEngine(app);
 initWebRoutes(app);
 
 connectDB();
+
 
 let port = process.env.PORT || 8080;
 app.listen(port, () => {
